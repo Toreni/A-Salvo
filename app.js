be519f8b43ed244cf6145955a882033fba8d8987
Iluvios <jdsub16@gmail.com>
2019-07-04 14:36:02 -0500

    Cambios finales

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+let DB = [];
+
+var cors = require('cors');
+// Then use it before your routes are set up:
+app.use(cors());
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
+});
+
+app.get('/administrador', function(req, res){
+  res.sendFile(__dirname + '/administrador.html');
+});
+
+app.post('/administrador', function(req, res){
+  console.log(req)
+  res.send("<H1> NOPE </H1>")
+  res.end()
 });
